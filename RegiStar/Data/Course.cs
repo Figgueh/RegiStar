d96673a9950d87a934a78b4c40ce5e8dc16eb45e
Iulii Turenco <1796175@johnabbottcollege.net>
2018-10-25 15:24:10 -0400

    Added Course Class and Academicdetails Design

---
+++ RegiStar/Data/Course.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegiStar.Data
+{
+    public class Course : INotifyPropertyChanged
+    {
+        //Variables.
+        private int _courseID;
+        private string _name;
+        private string _description;
+        private int _gradeID;
+        private int _isbn;
+        private int _studentID;
+        private int _timeID;
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Getters and Setters
+        public int CourseID
+        {
+            get { return this._courseID; }
+            set
+            {
+                _courseID = value;
+                OnPropertyChanged("CourseID");
+            }
+        }
+        public string Name
+        {
+            get { return this._name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Description
+        {
+            get { return this._description; }
+            set
+            {
+                _description = value;
+                OnPropertyChanged("Description");
+            }
+        }
+        public int GradeID
+        {
+            get { return this._gradeID; }
+            set
+            {
+                _gradeID = value;
+                OnPropertyChanged("GradeID");
+            }
+        }
+        public int Isbn
+        {
+            get { return this._isbn; }
+            set
+            {
+                _isbn = value;
+                OnPropertyChanged("Isbn");
+            }
+        }
+        public int StudentID
+        {
+            get { return this._studentID; }
+            set
+            {
+                _studentID = value;
+                OnPropertyChanged("StudentID");
+            }
+        }
+        public int TimeID
+        {
+            get { return this._timeID; }
+            set
+            {
+                _timeID = value;
+                OnPropertyChanged("TimeID");
+            }
+        }
+
+        //Main Course Constructor
+        public Course(int CourseID, string Name, string Description, int GradeID, int Isbn, int StudentID, int TimeID)
+        {
+            _courseID = CourseID;
+            _name = Name;
+            _description = Description;
+            _gradeID = GradeID;
+            _isbn = Isbn;
+            _studentID = StudentID;
+            _timeID = TimeID;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}, {1}, {2}, {3}, {4}, {5}, {5}", CourseID, Name, Description, GradeID, Isbn, StudentID, TimeID);
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
--- RegiStar/Windows/User.xaml.cs
+++ RegiStar/Windows/User.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RegiStar.Data;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,18 +13,91 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RegiStar.Data;
+using System.Data.SqlClient;
 
 namespace RegiStar.Windows
 {
     /// <summary>
     /// Interaction logic for User.xaml
     /// </summary>
+
+
+
+
+
+
     public partial class User : Window
     {
+
+        //Variables:
+        private ObservableCollection<Course> coursesList;
+
+        //Objects:
+        Course courses;
+
         public User()
         {
             InitializeComponent();
+            coursesList = new ObservableCollection<Course>();
+
+
+            //Get lists.
+            getCourses();
+
+            //Bind data to controls.
+            lstListOfClasses.ItemsSource = coursesList;
         }
+
+
+
+        private void getCourses()
+        {
+            //Setup connection to database.
+            using (SqlConnection conn = new SqlConnection("Data Source=DESKTOP-LSPNA6P;Initial Catalog=wpfRegistar;Integrated Security=True"))
+            {
+                //Open the connection.
+                conn.Open();
+
+                //Create query.
+                using (SqlCommand cmd = new SqlCommand("SELECT * FROM dbo.tblCourses", conn))
+                {
+
+                    //Setup reader to interpret the data.
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+
+                        //While reading the data:
+                        while (reader.Read())
+                        {
+                            //Create a new student with the data.
+                            try
+                            {
+                                courses = new Course(
+                                Convert.ToInt32(reader["courseID"]),
+                                reader["name"].ToString(),
+                                reader["description"].ToString(),
+                                Convert.ToInt32(reader["gradeID"]),
+                                Convert.ToInt32(reader["isbn"]),
+                                Convert.ToInt32(reader["studentID"]),
+                                Convert.ToInt32(reader["timeID"])
+                                );
+
+                                coursesList.Add(courses);
+                            }
+                            catch
+                            {
+                                MessageBox.Show("Failed to create course");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
 
         private void btnAddStudent_Click(object sender, RoutedEventArgs e)
         {
